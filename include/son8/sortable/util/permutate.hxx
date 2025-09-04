b4683f1a8d85ab1018b350e8bfc05f85401879f4
Oleg'Ease'Kharchuk <olegease@gmail.com>
2025-09-04 15:31:08 +0300

    Permutate 2025 09 04 (#11)
    * Permutate class enhance data initialization logic
    
    * add factors list and check_factor method for better maintainablility

--- include/son8/sortable/util/permutate.hxx
+++ include/son8/sortable/util/permutate.hxx
@@ -3,41 +3,59 @@
 
 #include <algorithm>
 #include <array>
+#include <initializer_list>
 
 namespace son8::sortable {
 
     template< typename Type, unsigned Size >
     class Permutate final {
+        static inline constexpr std::initializer_list< unsigned > factors_{ 2u, 3u, 4u, 5u, 8u };
+        static constexpr bool check_factor( unsigned n ) {
+            return ( Size % n == 0 && Size / n != 1 );
+        }
+        static constexpr unsigned permutate_outer_size( ) {
+            unsigned ret = Size;
+            for ( auto n : factors_ ) {
+                if ( check_factor( n ) ) ++ret;
+            }
+            return ret;
+        }
+        static constexpr unsigned const Inner_Size = Size;
+        static constexpr unsigned const Outer_Size = permutate_outer_size( );
         using ValueType = Type;
-        using ArrayType = std::array< ValueType, Size >;
-        using DataType = std::array< ArrayType, Size >;
+        using ArrayType = std::array< ValueType, Inner_Size >;
+        using DataType = std::array< ArrayType, Outer_Size >;
         DataType data_;
         ArrayType *curr_;
     public:
         Permutate( ) : curr_( &data_[0] ) {
-            unsigned step = 1u;
-            unsigned k = Size;
-            for ( unsigned outer = 0; outer < Size; ++outer ) {
+            // unsigned step = 1u;
+            unsigned k = Inner_Size;
+            unsigned outer = 0u;
+            for ( ; outer < Inner_Size; ++outer ) {
                 ValueType value{ };
                 data_[outer].fill( ++value );
-                if ( step == outer + 1 ) {
+
+                for ( unsigned inner = k; inner < Inner_Size; ++inner ) {
+                    data_[outer][inner] = ++value;
+                }
+                --k;
+            }
+            for ( auto n : factors_ ) {
+                if ( check_factor( n ) ) {
+                    ValueType value{ };
+                    data_[outer].fill( ++value );
+                    auto step = Inner_Size / n;
                     auto offset = step;
-                    while ( offset < Size ) {
+                    while ( offset < Inner_Size ) {
                         ++value;
-                        for ( auto inner = offset; inner < offset + step && inner < Size; ++inner ) {
+                        for ( auto inner = offset; inner < offset + step && inner < Inner_Size; ++inner ) {
                             data_[outer][inner] = value;
                         }
                         offset += step;
                     }
-                    step *= 2;
-                } else {
-                    for ( unsigned inner = k; inner < Size; ++inner ) {
-                        data_[outer][inner] = value;
-                        ++value;
-                    }
-
+                    ++outer;
                 }
-                --k;
             }
         }
 
@@ -47,7 +65,7 @@ namespace son8::sortable {
 
         bool next( ) {
             if ( std::next_permutation( curr_->begin( ), curr_->end( ) ) ) return true;
-            return ++curr_ > &data_[Size - 1] ? reset( ), false : true;
+            return ++curr_ > &data_[Outer_Size - 1] ? reset( ), false : true;
         }
 
         template< typename OutT >
